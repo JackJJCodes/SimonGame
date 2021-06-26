60bebaa8f92446e0c15e11ff6e4c271870f2319c
Jackson Jacob <sergiojacksonramos@gmail.com>
2021-06-26 22:56:20 +0530

    added all files

---
+++ game.js
@@ -0,0 +1,74 @@
+// Generating random number:
+
+let buttonColors = ["red", "blue", "green", "yellow"];
+
+let gamePattern = [];
+
+let userClickedPattern = [];
+
+let delayInMilliseconds = 100;
+
+let started = false; // This variable will be false at the beginning. After the game starts, it'll be true.
+
+let level = 0;
+
+// For getting keyboard input:
+$(document).keypress(function() {
+    if (!started)
+    {
+        $("#level-title").text("Level " + level);
+        nextSequence();
+        started = true;
+    }
+});
+
+
+$(".btn").on("click", function() {
+    // console.log("A button is pressed");
+    let userChosenColor = $(this).attr("id");
+    // console.log(userChosenColor);
+    userClickedPattern.push(userChosenColor);
+    // console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+})
+
+
+function nextSequence()
+{
+    level++;
+
+    $("level-title").text("Level " + level);
+
+    let randomNumber = Math.floor(Math.random() * 4);
+
+    let randomChosenColor = buttonColors[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColor);
+
+    animatePress(randomChosenColor);
+
+}
+
+function playSound(name)
+{
+    let audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColor)
+{
+    $("#" + currentColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + currentColor).removeClass("pressed");
+    }, delayInMilliseconds);
+}
+
+
+// **********************************
+
+
