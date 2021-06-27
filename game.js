9d9a9295afd87ade6db8d6c2549b23d3fc6fa770
Jackson Jacob <sergiojacksonramos@gmail.com>
2021-06-27 08:40:51 +0530

    added completed files

--- game.js
+++ game.js
@@ -1,15 +1,8 @@
-// Generating random number:
-
 let buttonColors = ["red", "blue", "green", "yellow"];
-
 let gamePattern = [];
-
 let userClickedPattern = [];
-
 let delayInMilliseconds = 100;
-
 let started = false; // This variable will be false at the beginning. After the game starts, it'll be true.
-
 let level = 0;
 
 // For getting keyboard input:
@@ -31,11 +24,44 @@ $(".btn").on("click", function() {
     // console.log(userClickedPattern);
     playSound(userChosenColor);
     animatePress(userChosenColor);
+
+    checkAnswer(userClickedPattern.length-1);
 })
 
+function checkAnswer(currentLevel)
+{
+    if (gamePattern[currentLevel] === userClickedPattern[currentLevel])
+    {
+        console.log("Success!");
+
+        if (userClickedPattern.length === gamePattern.length)
+        {
+            setTimeout(function() {
+                nextSequence();
+            }, 1000);
+        }
+    }
+    else
+    {
+        console.log("Failed!");
+
+        playSound("wrong");
+
+        $("body").addClass("game-over")
+        setTimeout(function() {
+            $("body").removeClass("game-over");
+        }, 200);
+
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+
+        startOver();
+    }
+}
 
 function nextSequence()
 {
+    userClickedPattern = [];
+
     level++;
 
     $("level-title").text("Level " + level);
@@ -50,7 +76,7 @@ function nextSequence()
 
     playSound(randomChosenColor);
 
-    animatePress(randomChosenColor);
+    // animatePress(randomChosenColor);
 
 }
 
@@ -69,6 +95,11 @@ function animatePress(currentColor)
 }
 
 
-// **********************************
+function startOver()
+{
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
 
 
